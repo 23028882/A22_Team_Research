9dcb7ae953b42c52ae29dbd3382ef4096615fdd4
Amal Parthasarathy Menon <ap23afp@herts.ac.uk>
2025-01-07 00:43:14 +0000

    Display first two rows and columns

---
+++ First 2 Columns and Row.R
@@ -0,0 +1,5 @@
+# Load the dataset (replace 'Time.csv' with the correct file path if necessary)
+dataset <- read.csv("C:/Users/hp/Downloads/A22_Team_Research/Time.csv")
+
+# Display the column headers and the first two rows
+head(dataset, 2)
